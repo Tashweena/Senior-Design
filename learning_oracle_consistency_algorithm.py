24181a5ce9a02a0d1a05e08e47498e36db811caa
Trishla Pokharna <trishla.pokharna@gmail.com>
2020-12-01 17:52:22 -0800

    Working Learning Oracle Consistency Algorithm

--- learning_oracle_consistency_algorithm.py
+++ learning_oracle_consistency_algorithm.py
@@ -1,7 +1,4 @@
-from sklearn.linear_model import SGDClassifier
-from sklearn.metrics import roc_auc_score
-import sys
-from sklearn.linear_model import LinearRegression, Ridge, LogisticRegression
+# custom loss function
 from scipy.optimize import minimize
 import numpy as np
 import sklearn
@@ -12,6 +9,7 @@ def sigmoid(z):
 
 def objective_function(weight, X, Y):
   loss = -1.0 * np.matmul(sigmoid(np.matmul(X,weight)), Y.T)/(len(X))
+
   return loss
 
 def linearClassification(X, Y):
@@ -22,17 +20,25 @@ def linearClassification(X, Y):
   result = minimize(objective_function, weight_init, args=(X,Y), method='CG', options={'maxiter': 800})
   return result.x
 
-def learning_oracle_consistency_algorithm(x_test, y_pred, y_test, alpha, delta):
+
+from sklearn.linear_model import SGDClassifier
+from sklearn.metrics import roc_auc_score
+import sys
+from sklearn.linear_model import LinearRegression, Ridge, LogisticRegression
+
+
+def learning_oracle_consistency_algorithm(x_test, y_pred, y_test, alpha):
   y_pred = y_pred.to_numpy()
   y_test = y_test.to_numpy()
   pos_resid = (y_pred - y_test)
   neg_resid =  -1 * pos_resid
+
   x_test = np.concatenate((x_test, np.ones((len(x_test), 1))), 1)
 
   beta1 = linearClassification(x_test, pos_resid) 
   y_test_resid_pos = sigmoid(np.matmul(x_test,beta1))
 
-  beta2 = linearClassification(x_test, neg_resid) 
+  beta2 = linearClassification(x_test, neg_resid)
   y_test_resid_neg = sigmoid(np.matmul(x_test,beta2))
 
   pos_high_error_set = []
@@ -40,8 +46,15 @@ def learning_oracle_consistency_algorithm(x_test, y_pred, y_test, alpha, delta):
 
   for idx in range(len(y_test_resid_pos)):
     if (y_test_resid_pos[idx] > .5):
-      pos_high_error_set.append(x_test[idx])
+      pos_high_error_set_indices.append(idx)
     if (y_test_resid_neg[idx] > .5):
-      neg_high_error_set.append(x_test[idx])
+      neg_high_error_set_indices.append(idx)
 
-  return pos_high_error_set, neg_high_error_set
+  return pos_high_error_set_indices, neg_high_error_set_indices, beta1, beta2
+
+
+
+
+
+
+
